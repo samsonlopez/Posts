9dae9122352d612c04d5a462adc84da02014f0c6
Samson Lopez <samsonlopez@samsons-macbook-pro.local>
2018-12-30 16:07:06 +0000

    Initial Commit

---
+++ Posts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Samson Lopez on 30/12/2018.
+//  Copyright © 2018 Samson Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
