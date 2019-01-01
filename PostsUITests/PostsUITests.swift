da38961f9ae7bfd088c6a4d13f2e32a80aa406c2
Samson Lopez <samsonlopez@samsons-macbook-pro.local>
2019-01-01 20:16:10 +0000

    Added two basic UI tests, for posts list load and post details selection and load.

--- PostsUITests/PostsUITests.swift
+++ PostsUITests/PostsUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Posts
 
 class PostsUITests: XCTestCase {
 
@@ -16,6 +17,33 @@ class PostsUITests: XCTestCase {
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+    }
+    
+    func test_PostsList_LoadsPostsSuccessfully() {
+        let app = XCUIApplication()
+        let cellCount = app.tables.cells.count
+        XCTAssertEqual(cellCount, 100, "List count not 100 as expected")
+    }
+
+    func test_PostDetailSelection_NavigatesToCorrectPostDetail() {
+        let app = XCUIApplication()
+        let postsTable = app.tables.element(boundBy: 0)
+        
+        // Second cell text (post title) for Post List
+        let secondCellElement = postsTable.cells.element(boundBy: 1).staticTexts.element(boundBy: 0)
+        let secondCellElementTitleText = secondCellElement.label
+        
+        // Select second row to open post detail
+        postsTable.cells.element(boundBy: 1).staticTexts.element(boundBy: 0).tap()
+
+        let detailsTable = app.tables.element(boundBy: 1)
+
+        // First cell text (post title) for Post Detail
+        let firstCellElement = detailsTable.cells.element(boundBy: 0).staticTexts.element(boundBy: 0)
+        let firstCellElementTitleText = firstCellElement.label
+        
+        // Check if title texts match
+        XCTAssertEqual(secondCellElementTitleText, firstCellElementTitleText, "Correct post details not shown")
     }
 
 }
